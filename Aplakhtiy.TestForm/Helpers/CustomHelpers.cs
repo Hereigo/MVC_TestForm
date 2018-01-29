8826436b4fd549c8d12369353c0d659aacb33e56
Hereigo <Hereigo@online.ua>
2018-01-29 22:36:10 +0200

    refactoring

--- Aplakhtiy.TestForm/Helpers/CustomHelpers.cs
+++ Aplakhtiy.TestForm/Helpers/CustomHelpers.cs
@@ -11,40 +11,43 @@ using System.Xml.Serialization;
 
 namespace Aplakhtiy.TestForm.Helpers
 {
-	public static class CustomHelpers
-	{
-		public static MvcHtmlString PhoneEditorFor<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression)
-		{
-			MvcHtmlString label = LabelExtensions.LabelFor(helper, expression, new { @class = "control-label col-md-2" });
-			MvcHtmlString editor = EditorExtensions.EditorFor(helper, expression, new { htmlAttributes = new { @class = "form-control" } });
-			MvcHtmlString validation = ValidationExtensions.ValidationMessageFor(helper, expression, null, new { @class = "text-danger" });
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString PhoneEditorFor<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression)
+        {
+            //MvcHtmlString label =
+            //    LabelExtensions.LabelFor(helper, expression, new { @class = "control-label col-md-2" });
+            MvcHtmlString editor =
+                EditorExtensions.EditorFor(helper, expression, new { htmlAttributes = new { @class = "form-control" } });
+            //MvcHtmlString validation =
+            //    ValidationExtensions.ValidationMessageFor(helper, expression, null, new { @class = "text-danger" });
 
-			// Build the input elements
-			TagBuilder editorDiv = new TagBuilder("div");
-			editorDiv.AddCssClass("form-control text-box single-line");
-			editorDiv.InnerHtml = editor.ToString();
+            // Build the input elements
+            TagBuilder editorDiv = new TagBuilder("div");
+            editorDiv.AddCssClass("col-md-10");
+            editorDiv.InnerHtml = editor.ToString();
 
-			// Build the validation message elements
-			TagBuilder validationSpan = new TagBuilder("span");
-			validationSpan.AddCssClass("col-md-10");
-			validationSpan.InnerHtml = validation.ToString();
+            // Build the validation message elements
+            //TagBuilder validationSpan = new TagBuilder("span");
+            //validationSpan.AddCssClass("text-danger field-validation-error");
+            //validationSpan.InnerHtml = validation.ToString();
 
-			TagBuilder validationDiv = new TagBuilder("div");
-			validationDiv.AddCssClass("field-validation-valid text-danger");
-			validationDiv.InnerHtml = validationSpan.ToString();
-			
-			// Combine all elements
-			StringBuilder html = new StringBuilder();
-			html.Append(label.ToString());
-			html.Append(editorDiv.ToString());
-			html.Append(validationDiv.ToString());
-			
-			// Build the outer div
-			TagBuilder outerDiv = new TagBuilder("div");
-			outerDiv.AddCssClass("form-group");
-			outerDiv.InnerHtml = html.ToString();
+            //TagBuilder validationDiv = new TagBuilder("div");
+            //validationDiv.AddCssClass("field-validation-valid text-danger");
+            //validationDiv.InnerHtml = validationSpan.ToString();
 
-			return MvcHtmlString.Create(outerDiv.ToString());
-		}
-	}
+            // Combine all elements
+            StringBuilder html = new StringBuilder();
+            //html.Append(label.ToString());
+            html.Append(editorDiv.ToString());
+            //html.Append(validation.ToString());
+
+            // Build the outer div
+            TagBuilder outerDiv = new TagBuilder("div");
+            outerDiv.AddCssClass("form-group");
+            outerDiv.InnerHtml = html.ToString();
+
+            return MvcHtmlString.Create(outerDiv.ToString());
+        }
+    }
 }
